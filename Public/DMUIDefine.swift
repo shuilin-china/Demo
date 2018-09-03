74bf13074cacbddfd042ec08bdcb4d880c79533e
税林 <shuilin@foryou56.com>
2018-09-03 18:35:59 +0800

    整理

---
+++ Public/DMUIDefine.swift
@@ -0,0 +1,13 @@
+//
+//  DMUIDefine.swift
+//  Demo
+//
+//  Created by Foryou on 2018/9/3.
+//  Copyright © 2018年 Foryou. All rights reserved.
+//
+
+import UIKit
+
+let  SCREEN_WIDTH = UIScreen.main.bounds.size.width
+
+let  SCREEN_HEIGH = UIScreen.main.bounds.size.height
